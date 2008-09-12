e3235343bca186b304f4c7b52719109cb8d296f8
pulkomandy <pulkomandy@3b09e2c4-80d7-11dd-8b6f-79bb0735e950>
2008-09-12 15:22:39 +0000

    Added iDSK by Sid.

---
+++ iDSK/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "iDSK"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Version number of package */
+#define VERSION "0.1"
+
+/* Define to 1 if the X Window System is missing or not being used. */
+/* #undef X_DISPLAY_MISSING */
