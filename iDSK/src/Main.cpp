5d7fc6ce4156b8ef452a87b2eb4bea7c41d3f494
pulkomandy <pulkomandy@3b09e2c4-80d7-11dd-8b6f-79bb0735e950>
2008-09-17 11:28:06 +0000

    iDSK : english translation (requested by nurgle)

--- iDSK/src/Main.cpp
+++ iDSK/src/Main.cpp
@@ -84,14 +84,14 @@ int main(int argc, char** argv) {
 
 	if(opts.options_remain())
 	{
-		cout << "Trop d'options !" << endl;
+		cout << "Unhandled option ! Check the syntax." << endl;
 		exit(EXIT_FAILURE);
 	}
 
 }//namespace getopt
 
     if ( ! IsDskSet ) {
-	    cerr << "Vous n'avez pas selectionné de fichier image DSK" << endl;
+	    cerr << "You did not select a DSK file to work with !" << endl;
 	    help();
     }
     else cerr << "DSK : " << DskFile <<  endl;
@@ -100,7 +100,7 @@ int main(int argc, char** argv) {
     {
     	if ( ! MyDsk.ReadDsk(DskFile))
 	{
-    		cerr<< "Erreur de lecture du fichier ("<< DskFile << ")."<<endl;
+    		cerr<< "Error reading file ("<< DskFile << ")."<<endl;
     		exit(EXIT_FAILURE);
     	}
     	if ( ! MyDsk.CheckDsk() ) {
@@ -111,9 +111,9 @@ int main(int argc, char** argv) {
 	for(vector<string>::iterator iter=AmsdosFileList.begin(); iter!=AmsdosFileList.end(); iter++)
 	{
 		char* amsdosF = GetNomAmsdos(basename((char *)(*iter).c_str()));			 
-		cerr << "Fichier Amsdos : " << amsdosF << endl;
+		cerr << "Amsdos file : " << amsdosF << endl;
 		if ( (Indice= MyDsk.FileIsIn( amsdosF ))<0) {
-			cerr << "Erreur Fichier : "<< amsdosF << " non trouve."<< endl;
+			cerr << "Error: File "<< amsdosF << " not found."<< endl;
 			exit(EXIT_FAILURE);
 		}
 		MyDsk.OnViewFic(Indice);
@@ -121,7 +121,7 @@ int main(int argc, char** argv) {
 		if ( ModeListBasic ) 
 			cout << ViewBasic( ) << endl;
 		else if ( ModeListDams )
-			cout << "Pas implementé pour le moment."<<endl;
+			cout << "Not yet coded ! Please try a newer version of iDSK ! Sorry !"<<endl;
 		else if ( ModeListHex ) {
 			MyDsk.Hexdecimal();
 			cout << Listing << endl;
@@ -136,34 +136,31 @@ int main(int argc, char** argv) {
    	MyDsk.FormatDsk( 9, 42 );
 	if (! MyDsk.WriteDsk(DskFile))
 	{
-		cerr <<  "Erreur Ecriture fichier " << DskFile << endl;
+		cerr <<  "Error writing file " << DskFile << endl;
 		exit(EXIT_FAILURE);
 	}
     }
 
     if ( ModeListDsk ) { // lire Dsk 
-	   cerr << "Mode : List Dsk " << endl;
-	   cerr <<  "------------------------------------" << endl;
 	   if ( ! MyDsk.ReadDsk(DskFile) ) {
-    		cerr<< "Erreur de lecture du fichier ("<< DskFile << ")."<<endl;
+    		cerr<< "Error reading file ("<< DskFile << ")."<<endl;
     		exit(EXIT_FAILURE);
     	}
     	if ( ! MyDsk.CheckDsk() ) {
-    		cerr <<"Fichier image non supporter ("<< DskFile << ")."<<endl;
+    		cerr <<"Unsupported dsk file ("<< DskFile << ")."<<endl;
     		exit(EXIT_FAILURE);
     	}
     	cout << MyDsk.ReadDskDir();
     }
 
     if ( ModeImportFile ) { // Ajouter fichiers sur dsk
- 	cerr << "Mode : Import fichier dans Dsk " << endl;
  	if ( ! MyDsk.ReadDsk(DskFile) ) {
-		cerr<< "Erreur de lecture du fichier ("<< DskFile << ")."<<endl;
+		cerr<< "Error reading file ("<< DskFile << ")."<<endl;
     		exit(EXIT_FAILURE);
     	}
 
     	if ( ! MyDsk.CheckDsk() ) {
-    		cerr <<"Fichier image non supporté ("<< DskFile << ")."<<endl;
+    		cerr <<"Unsupported dsk file ("<< DskFile << ")."<<endl;
     		exit(EXIT_FAILURE);
     	}
 
@@ -172,17 +169,13 @@ int main(int argc, char** argv) {
     		string amsdosfile = GetNomAmsdos( basename( (char*)(*iter).c_str()) );
     		int Indice;
     		if ( ( Indice = MyDsk.FileIsIn( amsdosfile ) ) != -1 && !Force_Overwrite) {
-    			cerr << "(" << amsdosfile <<") Fichier existe, voulez vous ajouter quand meme ? (O/Oui)(N/Non) :";
+    			cerr << "(" << amsdosfile <<") File exists, replace ? (Y/N) (try -f switch for autoreplace...):";
     			string answer ;
     			cin >> uppercase >> answer;
-    			if ( answer != "O" && answer != "N" ) {
-    				cerr << "Soyez plus explicite ;)."<<endl;
-    				continue;
-    			}
-    			if ( answer == "O" )
+    			if ( answer == "Y")
     				MyDsk.RemoveFile(Indice);
     			else {
-    				cerr<<"Import aborde, Dsk non modifiee."<<endl;
+    				cerr<<"Import cancelled, dsk unchanged."<<endl;
     				cout << MyDsk.ReadDskDir();
     				exit(EXIT_SUCCESS);
     			}
@@ -190,49 +183,47 @@ int main(int argc, char** argv) {
 		else if(Force_Overwrite)
 			MyDsk.RemoveFile(Indice);
 	
-    		cerr << "Fichier Amsdos : "<< *iter << endl;
+    		cerr << "Amsdos file : "<< *iter << endl;
 	
 		MyDsk.PutFileInDsk(*iter,AmsdosType,loadAdress,exeAdress,UserNumber,System_file,Read_only);
     	}
 	if ( MyDsk.WriteDsk (DskFile) )
 		cout << MyDsk.ReadDskDir(); 
-	else cerr<< "Erreur ecriture fichier : " << DskFile << endl;
+	else cerr<< "Error writing file : " << DskFile << endl;
     }
 
     if ( ModeRemoveFile ) {
-		cerr << "Mode : Effacement fichier dans la DSK"<<endl;
 		if ( ! MyDsk.ReadDsk( (char*)DskFile.c_str() ) ) {
-    			cerr<< "Erreur de lecture du fichier ("<< DskFile << ")."<<endl;
+    			cerr<< "Error reading file ("<< DskFile << ")."<<endl;
     			exit(EXIT_FAILURE);
     		}
     		if ( ! MyDsk.CheckDsk() ) {
-    			cerr <<"Fichier image non supporté ("<< DskFile << ")."<<endl;
+    			cerr <<"unsupported DSK file ("<< DskFile << ")."<<endl;
     			exit(EXIT_FAILURE);
     		}
     		int Indice;
 		for(vector<string>::iterator iter=AmsdosFileList.begin(); iter!=AmsdosFileList.end(); iter++)
 		{
 			char* amsdosF = GetNomAmsdos(basename( (char*)(*iter).c_str()));			 
-			cerr << "Fichier Amsdos : " << amsdosF << endl;
+			cerr << "Amsdos file : " << amsdosF << endl;
 			if ( (Indice= MyDsk.FileIsIn( amsdosF ))<0) {
-				cerr << "Erreur Fichier : "<< amsdosF << " non trouvé."<< endl;
+				cerr << "Error : file "<< amsdosF << " not found."<< endl;
 				exit(EXIT_FAILURE);
 			}
 			MyDsk.RemoveFile(Indice);
 			if ( MyDsk.WriteDsk ((char*)DskFile.c_str()) )
 				cout << MyDsk.ReadDskDir(); 
-			else cerr<< "Erreur ecriture fichier : " << (*iter) << endl;
+			else cerr<< "Error writing file " << (*iter) << endl;
 		}
    }
 
    if ( ModeGetFile ) {
-	cerr << "Mode : export fichier Amsdos " << endl;
 	if ( ! MyDsk.ReadDsk( (char*)DskFile.c_str() ) ) {
-    		cerr<< "Erreur de lecture du fichier ("<< DskFile << ")."<<endl;
+    		cerr<< "Error reading dskfile ("<< DskFile << ")."<<endl;
     		exit(EXIT_FAILURE);
     	}
     	if ( ! MyDsk.CheckDsk() ) {
-    		cerr <<"Fichier image non supporter ("<< DskFile << ")."<<endl;
+    		cerr <<"Unsupported dsk ("<< DskFile << ")."<<endl;
     		exit(EXIT_FAILURE);
     	}
     	int Indice;
@@ -242,11 +233,11 @@ int main(int argc, char** argv) {
     		char* amsdosF = GetNomAmsdos(basename( (char*)(*iter).c_str()));			 
 		cerr << "Fichier Amsdos : " << amsdosF << endl;
 		if ( (Indice= MyDsk.FileIsIn( amsdosF ))<0) {
-			cerr << "Erreur Fichier : "<< amsdosF << " non trouve."<< endl;
+			cerr << "Error : file "<< amsdosF << " not found."<< endl;
 			exit(EXIT_FAILURE);
 		}
 		if ( ! MyDsk.GetFileInDsk((char*)(*iter).c_str(),Indice) ) {
-			cerr <<"Erreur systeme, ne peut copier ("<<AmsdosFile<<")."<<endl;
+			cerr <<"System error : unable to copy ("<<AmsdosFile<<")."<<endl;
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -265,29 +256,30 @@ void help(void)
   cout <<endl;
   cout << "--------------------------------------------------------------------------------" << endl;
   cout << "################################################################################"<< endl;
-  cout <<  VERSION <<" (auteurs  : Demoniak, Sid, PulkoMandy), Contact SiD@Gmail.CoM" << endl;
+  cout <<  VERSION <<" (by Demoniak, Sid, PulkoMandy), www.code.google.com/p/cpcsdk " << endl;
   cout << "################################################################################"<< endl;
   cout << endl;
   cout << "Usage : " << endl;
-  cout << "\t"<< PROGNAME << " <fichier DSK> [OPTION] [fichiers a traiter]" << endl;
-  cout << "OPTIONS :" << endl;
-  cout << "-l : list le contenu du catalogue ex: -l mon_dsk.dsk" << endl;
-  cout << "-i : importe un fichier (-t type 0 pour un fichier ASCII et 1 pour un BINAIRE) ex: -i mon_fichier.bin -t 1 -s mon_fichier.dsk" << endl; 
-  cout << "-e : donne l'adresse d'execution du fichier (en hexadecimal) a inserer ex: -i mon_fichier.bin -e C000 -t 1 -s mon_fichier.dsk" << endl;
-  cout << "-c : donne l'adresse de chargement du fichier(en hexadecimal)  a inserer ex: -i mon_fichier.bin -e C000 -c 4000 -t 1 -s mon_fichier.dsk" << endl;
-  cout << "-g : exporte un fichier ex: -g mon-fichier.bas -s mon_fichie.dsk"<<endl;
-  cout << "-r : enlève un fichier" << endl;
-  cout << "-n : cree une nouvelle image dsk ex: -n mon_fichier.dsk" << endl;
-  cout << "-z : desassemble un binaire ex: -z mon-fichier.bin -s mon_fichier.dsk" << endl;
-  cout << "-b : liste un fichier Basic ex: -b mon-fichier.bas -s mon_fichier.dsk" << endl;
-  cout << "-d : liste un fichier Dams  ex: -d mon-fichier.dms -s mon_fichier.dsk" << endl;
-  cout << "-h : liste un fichier en Hexadecimale ex: -h mon-fichier.bin -s mon_fichier.dsk" << endl;
-  cout << "-f : force l'écrasement lorsqu'un fichier existe déjà" << endl
-       << "-o : fichier en lecture seule" << endl
-       << "-s : fichier système" << endl
-       << "-u : numéro d'utilisateur ex: -u 3" << endl;
+  cout << "\t"<< PROGNAME << " <DSKfile> [OPTIONS] [files to process]" << endl;
+  cout << "OPTIONS :                              EXAMPLE" << endl;
+  cout << "-l : List disk catalog                 iDSK floppy.dsk -l" << endl;
+  cout << "-g : export ('Get') file               iDSK floppy.dsk -g myprog.bas"<<endl;
+  cout << "-r : Remove file                       iDSK floppy.dsk -r myprog.bas" << endl;
+  cout << "-n : create New dsk file               iDSK floppy2.dsk -n" << endl;
+  cout << "-z : disassemble a binary file         iDSK floppy.dsk -z myprog.bin" << endl;
+  cout << "-b : list a Basic file                 iDSK floppy.dsk -b myprog.bas" << endl;
+  cout << "-d : list a Dams file                  iDSK floppy.dsk -d myprog.dms" << endl;
+  cout << "-h : list a binary file as Hexadecimal iDSK floppy.dsk -h myprog.bin" << endl;
+  cout << "-i : Import file                       iDSK floppy.dsk -i myprog.bas" << end;
+      <<  " -t : fileType (0=ASCII/1=BINARY)           ... -t 1" << endl; 
+  cout << " -e : hex Execute address of file           ... -e C000 -t 1" << endl;
+  cout << " -c : hex loading address of file           ... -e C000 -c 4000 -t 1" << endl;
+  cout << " -f : Force overwriting if file exists      ... -f" << endl
+       << " -o : insert a read-Only file               ... -o" << endl
+       << " -s : insert a System file                  ... -s" << endl
+       << " -u : insert file with User number          ... -u 3" << endl;
   cout << "--------------------------------------------------------------------------------" << endl;
-  cout << "*/\\/\\/SiD oF ImPAct/\\/\\/*" << endl;
+  cout << "Please report bugs ! - Demoniak/Sid/PulkoMandy" << endl;
   exit (0);
 }
 
