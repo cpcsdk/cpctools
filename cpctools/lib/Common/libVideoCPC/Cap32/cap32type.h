33c508f57dc9cd3f13ebd5383bafc0c26059b0a1
pulkomandy <pulkomandy@3b09e2c4-80d7-11dd-8b6f-79bb0735e950>
2008-09-17 19:30:42 +0000

    Modified some makefiles to help getting everything build with one make command. -libdsk should be removed if there are no local modifications versus the original libdsk 1.2.1. There are 3 versions of it anyways, one should be enough. Note there is a 1.3.0 development version on the original website. -all thise makefile.in should not be in the repository, they are generated during the build process _AntTweakBar is a pain to build, and chatting with cloudstrife pointed a function that does not have the same name depending on the videocard. Turned out the one in the repository is up to date and mine is outdated. So, i letf that as is.

--- cpctools/lib/Common/libVideoCPC/Cap32/cap32type.h
+++ cpctools/lib/Common/libVideoCPC/Cap32/cap32type.h
@@ -4,16 +4,18 @@
 
 #pragma once
 #define _CAP32TYPES_H_
+#include <stdint.h>
 
 typedef unsigned char byte;
 typedef unsigned short word;
 typedef unsigned int dword;
+/* Defined in stdint.h
 #if _WINDOWS
 typedef _int64 int64_t;
 #else
 typedef long long int64_t ;
 #endif
-
+*/ 
 typedef union {
 #if SDL_BYTEORDER == SDL_LIL_ENDIAN
    struct { byte l, h, h2, h3; } b;
