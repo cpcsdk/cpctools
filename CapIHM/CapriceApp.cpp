7aaa4250f9d222fd6755ee93759657c2c85cb7c9
giot.romain <giot.romain@3b09e2c4-80d7-11dd-8b6f-79bb0735e950>
2008-09-23 18:47:57 +0000

    * Add a splash screen * Add command line arguments

--- CapIHM/CapriceApp.cpp
+++ CapIHM/CapriceApp.cpp
@@ -1,13 +1,9 @@
 // For compilers that don't support precompilation, include "wx/wx.h"
-#include "wx/wxprec.h"
- 
-#ifndef WX_PRECOMP
-#	include "wx/wx.h"
-#endif
  
 #include "CapriceApp.h"
 #include "CapriceWindowImpl.h"
- 
+
+#include <wx/splash.h> 
 #include <zlib.h>
 #include <SDL.h>
 
@@ -33,18 +29,41 @@ IMPLEMENT_APP(CapriceApp)
 // This is executed upon startup, like 'main()' in non-wxWidgets programs.
 bool CapriceApp::OnInit()
 {
-	wxImage::AddHandler(new wxPNGHandler);
-	emulator = new Emulator();
+    // call default behaviour (mandatory)
+    if (!wxApp::OnInit())
+        return false;
 
+	wxImage::AddHandler(new wxPNGHandler);
+
+	//Splash screen managment
+	wxBitmap bitmap;
+	if (bitmap.LoadFile( wxT("logo.png"), wxBITMAP_TYPE_PNG))
+	{
+	 	wxSplashScreen* splash = new wxSplashScreen(bitmap,
+		wxSPLASH_CENTRE_ON_SCREEN|wxSPLASH_TIMEOUT,
+		6000, NULL, -1, wxDefaultPosition, wxDefaultSize,
+		wxSIMPLE_BORDER|wxSTAY_ON_TOP);
+	}
+	wxYield();
+
+
+	//Create emulator and IHM
+	emulator = new Emulator();
 
 	frame = new CapriceWindowImpl();
 	frame->Show(true);
 
 	SetTopWindow(frame);
 
-
-
 	return true ; 
+}
+
+/**
+ * @TODO write the code
+ */
+int CapriceApp::OnExit()
+{
+	return 0;
 }
 
 
@@ -58,6 +77,17 @@ int CapriceApp::OnRun() {
 	
 	if(emulator->Init()){
 		frame->SetEmulator(emulator);
+
+		//do the initialisations
+		if (!drivea.IsEmpty()) emulator->GetFDC().insertA( (const char *) drivea.char_str());
+		if (!driveb.IsEmpty()) emulator->GetFDC().insertB( (const char *) driveb.char_str());
+
+		if (!snapshot.IsEmpty()) snapshot_load( *emulator,  snapshot.char_str()) ;
+
+		if (!tape.IsEmpty()) emulator->GetTape().tape_insert(tape.char_str()) ; 
+	
+		if (fullscreen) emulator->GetRenderer().ToggleFullScreen();		
+
 	}
     // generate an initial idle event to start things
     wxIdleEvent event;
@@ -66,4 +96,28 @@ int CapriceApp::OnRun() {
 
     // start the main loop
     return wxApp::OnRun();
+}
+
+
+
+
+
+
+void CapriceApp::OnInitCmdLine(wxCmdLineParser& parser)
+{
+    parser.SetDesc (g_cmdLineDesc);
+    // must refuse '/' as parameter starter or cannot use "/path" style paths
+    parser.SetSwitchChars (wxT("-"));
+}
+ 
+bool CapriceApp::OnCmdLineParsed(wxCmdLineParser& parser)
+{
+    fullscreen = parser.Found(wxT("f"));
+	parser.Found(wxT("a"), &drivea);
+	parser.Found(wxT("b"), &driveb);
+	parser.Found(wxT("t"), &tape);
+	parser.Found(wxT("s"), &snapshot);
+
+
+    return true;
 }
--- CapIHM/CapriceApp.h
+++ CapIHM/CapriceApp.h
@@ -1,6 +1,13 @@
 #ifndef INCLUDED_CAPRICEAPP_H
 #define INCLUDED_CAPRICEAPP_H
 
+#include <wx/wxprec.h>
+#include <wx/cmdline.h>
+#ifndef WX_PRECOMP
+       #include <wx/wx.h>
+#endif
+ 
+
 #include "CapriceWindowImpl.h"
 #include "CapriceRegistersDialogImpl.h"
 #include "emulator.h"
@@ -11,14 +18,41 @@ class CapriceApp : public wxApp
 public:
 	virtual bool OnInit();
 	virtual int  OnRun();
+	virtual int  OnExit();
+    virtual void OnInitCmdLine(wxCmdLineParser& parser);
+    virtual bool OnCmdLineParsed(wxCmdLineParser& parser);
 
 
 private:
 	CapriceWindowImpl 	*frame ;
 	Emulator			*emulator ;
 
+	//params
+	bool		fullscreen ;
+	wxString	drivea ;
+	wxString	driveb ;
+	wxString	tape ;
+	wxString 	snapshot ;	
+
 };
  
+
+static const wxCmdLineEntryDesc g_cmdLineDesc [] =
+{
+     { wxCMD_LINE_SWITCH, wxT("h"), wxT("help"), wxT("displays help on the command line parameters"), wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_HELP },
+     { wxCMD_LINE_SWITCH, wxT("f"), wxT("fullscreen"), wxT("display Caprice in full screen") },
+     { wxCMD_LINE_OPTION, wxT("a"), wxT("drivea"), wxT("specify disk image to insert into drive A"), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL  },
+ 
+     { wxCMD_LINE_OPTION, wxT("b"), wxT("driveb"), wxT("specify disk image to insert into drive B"), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL  },
+     
+     { wxCMD_LINE_OPTION, wxT("t"), wxT("tape"), wxT("specify tape image "), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL  },
+     { wxCMD_LINE_OPTION, wxT("s"), wxT("snapshot"), wxT("specify snapshot image "), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL  },
+
+	{ wxCMD_LINE_NONE }
+};
+ 
+
+
 DECLARE_APP(CapriceApp)
  
 #endif // INCLUDED_CAPRICEAPP_H 
