8e9275e166db92a0a05da20345a48df3acec093c
pulkomandy <pulkomandy@3b09e2c4-80d7-11dd-8b6f-79bb0735e950>
2009-04-15 20:35:42 +0000

    Made some tables const in the z80 emulation.

--- CapIHM/src/core/z80daa.h
+++ CapIHM/src/core/z80daa.h
@@ -29,7 +29,7 @@
 #define ZF	0x40
 #define SF	0x80
 
-static word DAATable[0x800] = {
+static const word DAATable[0x800] = {
   (0x00<<8)   +ZF         +VF      ,
   (0x01<<8)                        ,
   (0x02<<8)                        ,
