e0fe53e5ff1697a8f831d9383f0d15a9f15f4930
pulkomandy <pulkomandy@3b09e2c4-80d7-11dd-8b6f-79bb0735e950>
2010-06-02 20:14:33 +0000

    * Add a menu entry "Reload Snapshot"  * Add shortcuts to the snapshot menu entries  * Remove old unused shortcut handler

--- Reloaded/src/gui/WXEmulator.cpp
+++ Reloaded/src/gui/WXEmulator.cpp
@@ -127,102 +127,14 @@ void WXEmulator::ReleaseKey(uint32_t key, uint32_t mod)
 	cpc_key = _input.keyboard_normal[key];
 
 	// a key of the CPC keyboard?
-	if (!(cpc_key & MOD_EMU_KEY))
+	if ((!_config.paused) && ((byte)cpc_key != 0xff))
 	{
-		if ((!_config.paused) && ((byte)cpc_key != 0xff))
-		{
-			// key has been released
-			_input.keyboard_matrix[(byte)cpc_key >> 4] |= (1 << ((byte)cpc_key & 7));
-			// make sure key is unSHIFTed
-			_input.keyboard_matrix[0x25 >> 4] |= (1 << (0x25 & 7));
-			// make sure CONTROL key is not held down
-			_input.keyboard_matrix[0x27 >> 4] |= (1 << (0x27 & 7));
-		}
-	}
-	// process emulator specific keys
-	else
-	{
-		switch (cpc_key)
-		{
-			case CAP32_FULLSCRN:
-				{
-					audio_pause(_config);
-
-					if (!_renderer.ToggleFullScreen())
-					{
-						fprintf(stderr, "video_init() failed. Aborting.\n");
-						exit(-1);
-					}
-
-					audio_resume(_config);
-					break;
-				}
-
-			case CAP32_TAPEPLAY:
-				{
-					if (pbTapeImage)
-					{
-						if (_config.tape_play_button)
-						{
-							_config.tape_play_button = 0;
-						}
-						else
-						{
-							_config.tape_play_button = 0x10;
-						}
-					}
-					break;
-				}
-
-			case CAP32_RESET:
-				{
-					emulator_reset(false);
-					break;
-				}
-
-			case CAP32_JOY:
-				{
-					_config.joysticks = _config.joysticks ? 0 : 1;
-					_input.input_swap_joy(_config);
-					break;
-				}
-
-			case CAP32_EXIT:
-				{
-					exitRequested = true;
-					break;
-				}
-
-			case CAP32_FPS:
-				{
-					FPSDisplay = !FPSDisplay;
-					break;
-				}
-
-			case CAP32_SPEED:
-				{
-					_config.limit_speed = _config.limit_speed ? 0 : 1;
-					//fprintf(stderr, "Toggle limit speed\n");
-					break;
-				}
-
-#ifdef USE_DEBUGGER
-			case CAP32_DEBUG:
-				{
-					dwDebugFlag = dwDebugFlag ? 0 : 1;
-#ifdef USE_DEBUGGER_CRTC
-					if (!dwDebugFlag)
-						break;
-
-					for (int n = 0; n < 14; n++)
-					{
-						fprintf(pfoDebug, "CRTC Reg %02x = %02x\r\n", n, _crtc->GetRegisterValue( n ));
-					}
-#endif
-					break;
-				}
-#endif
-		}
+		// key has been released
+		_input.keyboard_matrix[(byte)cpc_key >> 4] |= (1 << ((byte)cpc_key & 7));
+		// make sure key is unSHIFTed
+		_input.keyboard_matrix[0x25 >> 4] |= (1 << (0x25 & 7));
+		// make sure CONTROL key is not held down
+		_input.keyboard_matrix[0x27 >> 4] |= (1 << (0x27 & 7));
 	}
 }
 
