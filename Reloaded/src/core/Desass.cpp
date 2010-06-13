2b2858492fb9d62468aefcbd0bcabdf43d572d2b
pulkomandy <pulkomandy@3b09e2c4-80d7-11dd-8b6f-79bb0735e950>
2010-06-13 15:35:46 +0000

    * Fix spacing for 4-bytes opcodes  * Fix egative 8bit number having a lot of useless digits

--- Reloaded/src/core/Desass.cpp
+++ Reloaded/src/core/Desass.cpp
@@ -120,21 +120,21 @@ const char * TabInstrED[ 256 ] =
 	0,0,0,0,
 	0,0,0,0,
 	0,0,0,0,
-	"IN B,(C)","OUT (C),B","SBC HL,BC","LD (0x%04X),BC", // 40
+	"IN B,(C)","OUT (C),B","SBC HL,BC","LD (#%0.4X),BC", // 40
 	"NEG","RETN","IM 0","LD I,A",
-	"IN C,(C)","OUT (C),C","ADC HL,BC","LD BC,(0x%04X)",
+	"IN C,(C)","OUT (C),C","ADC HL,BC","LD BC,(#%0.4X)",
 	0,"RETI",0,"LD R,A",
-	"IN D,(C)","OUT (C),D","SBC HL,DE","LD (0x%04X),DE", // 50
+	"IN D,(C)","OUT (C),D","SBC HL,DE","LD (#%0.4X),DE", // 50
 	0,0,"IM 1","LD A,I",
-	"IN E,(C)","OUT (C),E","ADC HL,DE","LD DE,(0x%04X)",
+	"IN E,(C)","OUT (C),E","ADC HL,DE","LD DE,(#%0.4X)",
 	0,0,"IM 2","LD A,R",
 	"IN H,(C)","OUT (C),H","SBC HL,HL",0, // 60
 	0,0,0,"RRD",
 	"IN L,(C)","OUT (C),L","ADC HL,HL",0,
 	0,0,0,"RLD",
-	0,"OUT (C),0","SBC HL,SP","LD (0x%04X),SP", // 70
+	0,"OUT (C),0","SBC HL,SP","LD (#%0.4X),SP", // 70
 	0,0,0,0,
-	"IN A,(C)","OUT (C),A","ADC HL,SP","LD SP,(0x%04X)",
+	"IN A,(C)","OUT (C),A","ADC HL,SP","LD SP,(#%0.4X)",
 	0,0,0,0,
 	0,0,0,0,
 	0,0,0,0,
@@ -181,46 +181,46 @@ const char * TabInstrDD[ 256 ] =
 	0,0,0,0,
 	0,"ADD IX,DE",0,0,
 	0,0,0,0,
-	0,"LD IX,0x%04X","LD (0x%04X),IX","INC IX",
-	"INC IXh","DEC IXh","LD IXh,0x%02X",0,
-	0,"ADD IX,HL","LD IX,(0x%04X)","DEC IX",
-	"INC IXl","DEC IXl","LD IXl,0x%02X",0,
+	0,"LD IX,#%0.4X","LD (#%0.4X),IX","INC IX",
+	"INC IXh","DEC IXh","LD IXh,#%2.2X",0,
+	0,"ADD IX,HL","LD IX,(#%0.4X)","DEC IX",
+	"INC IXl","DEC IXl","LD IXl,#%2.2X",0,
 	0,0,0,0,
-	"INC (IX+%02X)","DEC (IX+%02X)","LD (IX+%02X),0x%02X",0,
+	"INC (IX+#%2.2X)","DEC (IX+#%2.2X)","LD (IX+#%2.2X),#%2.2X",0,
 	0,"ADD IX,SP",0,0,
 	0,0,0,0,
 	0,0,0,0,
-	"LD B,IXh","LD B,IXl","LD B,(IX+%02X)",0,
+	"LD B,IXh","LD B,IXl","LD B,(IX+#%2.2X)",0,
 	0,0,0,0,
-	"LD C,IXh","LD C,IXl","LD C,(IX+%02X)",0,
+	"LD C,IXh","LD C,IXl","LD C,(IX+#%2.2X)",0,
 	0,0,0,0,
-	"LD D,IXh","LD D,IXl","LD D,(IX+%02X)",0,
+	"LD D,IXh","LD D,IXl","LD D,(IX+#%2.2X)",0,
 	0,0,0,0,
-	"LD E,IXh","LD E,IXl","LD E,(IX+%02X)",0,
+	"LD E,IXh","LD E,IXl","LD E,(IX+#%2.2X)",0,
 	"LD IXh,B","LD IXh,C","LD IXh,D","LD IXh,E",
-	"LD IXh,IXh","LD IXh,IXl","LD H,(IX+%02X)","LD IXh,A",
+	"LD IXh,IXh","LD IXh,IXl","LD H,(IX+#%2.2X)","LD IXh,A",
 	"LD IXl,B","LD IXl,C","LD IXl,D","LD IXl,E",
-	"LD IXl,IXh","LD IXl,IXl","LD L,(IX+%02X)","LD IXl,A",
-	"LD (IX+%02X),B","LD (IX+%02X),C","LD (IX+%02X),D","LD (IX+%02X),E",
-	"LD (IX+%02X),H","LD (IX+%02X),L",0,"LD (IX+%02X),A",
+	"LD IXl,IXh","LD IXl,IXl","LD L,(IX+#%2.2X)","LD IXl,A",
+	"LD (IX+#%2.2X),B","LD (IX+#%2.2X),C","LD (IX+#%2.2X),D","LD (IX+#%2.2X),E",
+	"LD (IX+#%2.2X),H","LD (IX+#%2.2X),L",0,"LD (IX+#%2.2X),A",
 	0,0,0,0,
-	"LD A,IXh","LD A,IXl","LD A,(IX+%02X)",0,
+	"LD A,IXh","LD A,IXl","LD A,(IX+#%2.2X)",0,
 	0,0,0,0,
-	"ADD A,IXh","ADD A,IXl","ADD A,(IX+%02X)",0,
+	"ADD A,IXh","ADD A,IXl","ADD A,(IX+#%2.2X)",0,
 	0,0,0,0,
-	"ADC A,IXh","ADC A,IXl","ADC A,(IX+%02X)",0,
+	"ADC A,IXh","ADC A,IXl","ADC A,(IX+#%2.2X)",0,
 	0,0,0,0,
-	"SUB IXh","SUB IXl","SUB (IX+%02X)",0,
+	"SUB IXh","SUB IXl","SUB (IX+#%2.2X)",0,
 	0,0,0,0,
-	"SBC A,IXh","SBC A,IXl","SBC A,(IX+%02X)",0,
+	"SBC A,IXh","SBC A,IXl","SBC A,(IX+#%2.2X)",0,
 	0,0,0,0,
-	"AND IXh","AND IXl","AND (IX+%02X)",0,
+	"AND IXh","AND IXl","AND (IX+#%2.2X)",0,
 	0,0,0,0,
-	"XOR IXh","XOR IXl","XOR (IX+%02X)",0,
+	"XOR IXh","XOR IXl","XOR (IX+#%2.2X)",0,
 	0,0,0,0,
-	"OR IXh","OR IXl","OR (IX+%02X)",0,
+	"OR IXh","OR IXl","OR (IX+#%2.2X)",0,
 	0,0,0,0,
-	"CP IXh","CP IXl","CP (IX+%02X)",0,
+	"CP IXh","CP IXl","CP (IX+#%2.2X)",0,
 	0,0,0,0,
 	0,0,0,0,
 	0,0,0,0,
@@ -242,38 +242,38 @@ const char * TabInstrDD[ 256 ] =
 
 const char * TabInstrDDCB[ 256 ] =
 {
-	0,0,0,0,0,0,"RLC (IX+%02X)",0,
-	0,0,0,0,0,0,"RRC (IX+%02X)",0,
-	0,0,0,0,0,0,"RL (IX+%02X)",0,
-	0,0,0,0,0,0,"RR (IX+%02X)",0,
-	0,0,0,0,0,0,"SLA (IX+%02X)",0,
-	0,0,0,0,0,0,"SRA (IX+%02X)",0,
-	0,0,0,0,0,0,"SLL (IX+%02X)",0,
-	0,0,0,0,0,0,"SRL (IX+%02X)",0,
-	0,0,0,0,0,0,"BIT 0,(IX+%02X)",0,
-	0,0,0,0,0,0,"BIT 1,(IX+%02X)",0,
-	0,0,0,0,0,0,"BIT 2,(IX+%02X)",0,
-	0,0,0,0,0,0,"BIT 3,(IX+%02X)",0,
-	0,0,0,0,0,0,"BIT 4,(IX+%02X)",0,
-	0,0,0,0,0,0,"BIT 5,(IX+%02X)",0,
-	0,0,0,0,0,0,"BIT 6,(IX+%02X)",0,
-	0,0,0,0,0,0,"BIT 7,(IX+%02X)",0,
-	0,0,0,0,0,0,"RES 0,(IX+%02X)",0,
-	0,0,0,0,0,0,"RES 1,(IX+%02X)",0,
-	0,0,0,0,0,0,"RES 2,(IX+%02X)",0,
-	0,0,0,0,0,0,"RES 3,(IX+%02X)",0,
-	0,0,0,0,0,0,"RES 4,(IX+%02X)",0,
-	0,0,0,0,0,0,"RES 5,(IX+%02X)",0,
-	0,0,0,0,0,0,"RES 6,(IX+%02X)",0,
-	0,0,0,0,0,0,"RES 7,(IX+%02X)",0,
-	0,0,0,0,0,0,"SET 0,(IX+%02X)",0,
-	0,0,0,0,0,0,"SET 1,(IX+%02X)",0,
-	0,0,0,0,0,0,"SET 2,(IX+%02X)",0,
-	0,0,0,0,0,0,"SET 3,(IX+%02X)",0,
-	0,0,0,0,0,0,"SET 4,(IX+%02X)",0,
-	0,0,0,0,0,0,"SET 5,(IX+%02X)",0,
-	0,0,0,0,0,0,"SET 6,(IX+%02X)",0,
-	0,0,0,0,0,0,"SET 7,(IX+%02X)",0
+	0,0,0,0,0,0,"RLC (IX+%2.2X)",0,
+	0,0,0,0,0,0,"RRC (IX+%2.2X)",0,
+	0,0,0,0,0,0,"RL (IX+%2.2X)",0,
+	0,0,0,0,0,0,"RR (IX+%2.2X)",0,
+	0,0,0,0,0,0,"SLA (IX+%2.2X)",0,
+	0,0,0,0,0,0,"SRA (IX+%2.2X)",0,
+	0,0,0,0,0,0,"SLL (IX+%2.2X)",0,
+	0,0,0,0,0,0,"SRL (IX+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 0,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 1,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 2,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 3,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 4,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 5,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 6,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 7,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"RES 0,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"RES 1,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"RES 2,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"RES 3,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"RES 4,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"RES 5,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"RES 6,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"RES 7,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"SET 0,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"SET 1,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"SET 2,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"SET 3,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"SET 4,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"SET 5,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"SET 6,(IX+%2.2X)",0,
+	0,0,0,0,0,0,"SET 7,(IX+%2.2X)",0
 };
 
 
@@ -283,26 +283,26 @@ const char * TabInstrFD[ 256 ] =
 	0,"ADD IY,BC",0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,
 	0,"ADD IY,DE",0,0,0,0,0,0,
-	0,"LD IY,0x%04X","LD (0x%04X),IY","INC IY","INC IYh","DEC IYh","LD IYh,0x%02X",0,
-	0,"ADD IY,HL","LD IY,(0x%04X)","DEC IY","INC IYl","DEC IYl","LD IYl,0x%02X",0,
-	0,0,0,0,"INC (IY+%02X)","DEC (IY+%02X)","LD (IY+%02X),0x%02X",0,
+	0,"LD IY,#%.4X","LD (#%.4X),IY","INC IY","INC IYh","DEC IYh","LD IYh,#%2.2X",0,
+	0,"ADD IY,HL","LD IY,(#%.4X)","DEC IY","INC IYl","DEC IYl","LD IYl,#%2.2X",0,
+	0,0,0,0,"INC (IY+%2.2X)","DEC (IY+%2.2X)","LD (IY+%2.2X),#%2.2X",0,
 	0,"ADD IY,SP",0,0,0,0,0,0,
-	0,0,0,0,"LD B,IYh","LD B,IYl","LD B,(IY+%02X)",0,
-	0,0,0,0,"LD C,IYh","LD C,IYl","LD C,(IY+%02X)",0,
-	0,0,0,0,"LD D,IYh","LD D,IYl","LD D,(IY+%02X)",0,
-	0,0,0,0,"LD E,IYh","LD E,IYl","LD E,(IY+%02X)",0,
-	"LD IYh,B","LD IYh,C","LD IYh,D","LD IYh,E","LD IYh,IYh","LD IYh,IYl","LD H,(IY+%02X)","LD IYh,A",
-	"LD IYl,B","LD IYl,C","LD IYl,D","LD IYl,E","LD IYl,IYh","LD IYl,IYl","LD L,(IY+%02X)","LD IYl,A",
-	"LD (IY+%02X),B","LD (IY+%02X),C","LD (IY+%02X),D","LD (IY+%02X),E","LD (IY+%02X),H","LD (IY+%02X),L",0,"LD (IY+%02X),A",
-	0,0,0,0,"LD A,IYh","LD A,IYl","LD A,(IY+%02X)",0,
-	0,0,0,0,"ADD A,IYh","ADD A,IYl","ADD A,(IY+%02X)",0,
-	0,0,0,0,"ADC A,IYh","ADC A,IYl","ADC A,(IY+%02X)",0,
-	0,0,0,0,"SUB IYh","SUB IYl","SUB (IY+%02X)",0,
-	0,0,0,0,"SBC A,IYh","SBC A,IYl","SBC A,(IY+%02X)",0,
-	0,0,0,0,"AND IYh","AND IYl","AND (IY+%02X)",0,
-	0,0,0,0,"XOR IYh","XOR IYl","XOR (IY+%02X)",0,
-	0,0,0,0,"OR IYh","OR IYl","OR (IY+%02X)",0,
-	0,0,0,0,"CP IYh","CP IYl","CP (IY+%02X)",0,
+	0,0,0,0,"LD B,IYh","LD B,IYl","LD B,(IY+%2.2X)",0,
+	0,0,0,0,"LD C,IYh","LD C,IYl","LD C,(IY+%2.2X)",0,
+	0,0,0,0,"LD D,IYh","LD D,IYl","LD D,(IY+%2.2X)",0,
+	0,0,0,0,"LD E,IYh","LD E,IYl","LD E,(IY+%2.2X)",0,
+	"LD IYh,B","LD IYh,C","LD IYh,D","LD IYh,E","LD IYh,IYh","LD IYh,IYl","LD H,(IY+%2.2X)","LD IYh,A",
+	"LD IYl,B","LD IYl,C","LD IYl,D","LD IYl,E","LD IYl,IYh","LD IYl,IYl","LD L,(IY+%2.2X)","LD IYl,A",
+	"LD (IY+%2.2X),B","LD (IY+%2.2X),C","LD (IY+%2.2X),D","LD (IY+%2.2X),E","LD (IY+%2.2X),H","LD (IY+%2.2X),L",0,"LD (IY+%2.2X),A",
+	0,0,0,0,"LD A,IYh","LD A,IYl","LD A,(IY+%2.2X)",0,
+	0,0,0,0,"ADD A,IYh","ADD A,IYl","ADD A,(IY+%2.2X)",0,
+	0,0,0,0,"ADC A,IYh","ADC A,IYl","ADC A,(IY+%2.2X)",0,
+	0,0,0,0,"SUB IYh","SUB IYl","SUB (IY+%2.2X)",0,
+	0,0,0,0,"SBC A,IYh","SBC A,IYl","SBC A,(IY+%2.2X)",0,
+	0,0,0,0,"AND IYh","AND IYl","AND (IY+%2.2X)",0,
+	0,0,0,0,"XOR IYh","XOR IYl","XOR (IY+%2.2X)",0,
+	0,0,0,0,"OR IYh","OR IYl","OR (IY+%2.2X)",0,
+	0,0,0,0,"CP IYh","CP IYl","CP (IY+%2.2X)",0,
 	0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,
@@ -316,59 +316,59 @@ const char * TabInstrFD[ 256 ] =
 
 const char * TabInstrFDCB[ 256 ] =
 {
-	0,0,0,0,0,0,"RLC (IY+%02X)",0,
-	0,0,0,0,0,0,"RRC (IY+%02X)",0,
-	0,0,0,0,0,0,"RL (IY+%02X)",0,
-	0,0,0,0,0,0,"RR (IY+%02X)",0,
-	0,0,0,0,0,0,"SLA (IY+%02X)",0,
-	0,0,0,0,0,0,"SRA (IY+%02X)",0,
-	0,0,0,0,0,0,"SLL (IY+%02X)",0,
-	0,0,0,0,0,0,"SRL (IY+%02X)",0,
-	0,0,0,0,0,0,"BIT 0,(IY+%02X)",0,
-	0,0,0,0,0,0,"BIT 1,(IY+%02X)",0,
-	0,0,0,0,0,0,"BIT 2,(IY+%02X)",0,
-	0,0,0,0,0,0,"BIT 3,(IY+%02X)",0,
-	0,0,0,0,0,0,"BIT 4,(IY+%02X)",0,
-	0,0,0,0,0,0,"BIT 5,(IY+%02X)",0,
-	0,0,0,0,0,0,"BIT 6,(IY+%02X)",0,
-	0,0,0,0,0,0,"BIT 7,(IY+%02X)",0,
-	0,0,0,0,0,0,"RES 0,(IY+%02X)",0,
-	0,0,0,0,0,0,"RES 1,(IY+%02X)",0,
-	0,0,0,0,0,0,"RES 2,(IY+%02X)",0,
-	0,0,0,0,0,0,"RES 3,(IY+%02X)",0,
-	0,0,0,0,0,0,"RES 4,(IY+%02X)",0,
-	0,0,0,0,0,0,"RES 5,(IY+%02X)",0,
-	0,0,0,0,0,0,"RES 6,(IY+%02X)",0,
-	0,0,0,0,0,0,"RES 7,(IY+%02X)",0,
-	0,0,0,0,0,0,"SET 0,(IY+%02X)",0,
-	0,0,0,0,0,0,"SET 1,(IY+%02X)",0,
-	0,0,0,0,0,0,"SET 2,(IY+%02X)",0,
-	0,0,0,0,0,0,"SET 3,(IY+%02X)",0,
-	0,0,0,0,0,0,"SET 4,(IY+%02X)",0,
-	0,0,0,0,0,0,"SET 5,(IY+%02X)",0,
-	0,0,0,0,0,0,"SET 6,(IY+%02X)",0,
-	0,0,0,0,0,0,"SET 7,(IY+%02X)",0
+	0,0,0,0,0,0,"RLC (IY+%2.2X)",0,
+	0,0,0,0,0,0,"RRC (IY+%2.2X)",0,
+	0,0,0,0,0,0,"RL (IY+%2.2X)",0,
+	0,0,0,0,0,0,"RR (IY+%2.2X)",0,
+	0,0,0,0,0,0,"SLA (IY+%2.2X)",0,
+	0,0,0,0,0,0,"SRA (IY+%2.2X)",0,
+	0,0,0,0,0,0,"SLL (IY+%2.2X)",0,
+	0,0,0,0,0,0,"SRL (IY+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 0,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 1,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 2,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 3,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 4,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 5,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 6,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"BIT 7,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"RES 0,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"RES 1,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"RES 2,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"RES 3,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"RES 4,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"RES 5,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"RES 6,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"RES 7,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"SET 0,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"SET 1,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"SET 2,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"SET 3,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"SET 4,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"SET 5,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"SET 6,(IY+%2.2X)",0,
+	0,0,0,0,0,0,"SET 7,(IY+%2.2X)",0
 };
 
 
 const char * TabInstr[ 256 ] =
 {
-	"NOP","LD BC,0x%04X","LD (BC),A","INC BC", // 0
-	"INC B","DEC B","LD B,0x%02X","RLCA", // 4
+	"NOP","LD BC,#%.4X","LD (BC),A","INC BC", // 0
+	"INC B","DEC B","LD B,#%2.2X","RLCA", // 4
 	"EX AF,AF","ADD HL,BC","LD A,(BC)","DEC BC",
-	"INC C","DEC C","LD C,0x%02X","RRCA",
-	"DJNZ 0xeeee ","LD DE,0x%04X","LD (DE),A","INC DE", // 10
-	"INC D","DEC D","LD D,0x%02X","RLA",
-	"JR 0xeeee ","ADD HL,DE","LD A,(DE)","DEC DE",
-	"INC E","DEC E","LD E,0x%02X","RRA",
-	"JR NZ,0xeeee ","LD HL,0x%04X","LD (0x%04X),HL","INC HL", // 20
-	"INC H","DEC H","LD H,0x%02X","DAA",
-	"JR Z,0xeeee ","ADD HL,HL","LD HL,(0x%04X)","DEC HL",
-	"INC L","DEC L","LD L,0x%02X","CPL",
-	"JR NC,0xeeee ","LD SP,0x%04X","LD (0x%04X),A","INC SP", // 30
-	"INC (HL)","DEC (HL)","LD (HL),0x%02X","SCF",
-	"JR C,0xeeee ","ADD HL,SP","LD A,(0x%04X)","DEC SP",
-	"INC A","DEC A","LD A,0x%02X","CCF",
+	"INC C","DEC C","LD C,#%2.2X","RRCA",
+	"DJNZ #eeee ","LD DE,#%.4X","LD (DE),A","INC DE", // 10
+	"INC D","DEC D","LD D,#%2.2X","RLA",
+	"JR #eeee ","ADD HL,DE","LD A,(DE)","DEC DE",
+	"INC E","DEC E","LD E,#%2.2X","RRA",
+	"JR NZ,#eeee ","LD HL,#%.4X","LD (#%.4X),HL","INC HL", // 20
+	"INC H","DEC H","LD H,#%2.2X","DAA",
+	"JR Z,#eeee ","ADD HL,HL","LD HL,(#%.4X)","DEC HL",
+	"INC L","DEC L","LD L,#%2.2X","CPL",
+	"JR NC,#eeee ","LD SP,#%.4X","LD (#%.4X),A","INC SP", // 30
+	"INC (HL)","DEC (HL)","LD (HL),#%2.2X","SCF",
+	"JR C,#eeee ","ADD HL,SP","LD A,(#%.4X)","DEC SP",
+	"INC A","DEC A","LD A,#%2.2X","CCF",
 	"LD B,B","LD B,C","LD B,D","LD B,E", // 40
 	"LD B,H","LD B,L","LD B,(HL)","LD B,A",
 	"LD C,B","LD C,C","LD C,D","LD C,E",
@@ -401,22 +401,22 @@ const char * TabInstr[ 256 ] =
 	"OR H","OR L","OR (HL)","OR A",
 	"CP B","CP C","CP D","CP E",
 	"CP H","CP L","CP (HL)","CP A",
-	"RET NZ","POP BC","JP NZ,0x%04X","JP 0x%04X", // C0
-	"CALL NZ,0x%04X","PUSH BC","ADD A,0x%02X","RST 00",
-	"RET Z","RET","JP Z,0x%04X",0,
-	"CALL Z,0x%04X","CALL 0x%04X","ADC A,0x%02X","RST 08",
-	"RET NC","POP DE","JP NC,0x%04X","OUT (0x%02X),A", // D0
-	"CALL NC,0x%04X","PUSH DE","SUB 0x%02X","RST 10",
-	"RET C","EXX","JP C,0x%04X","IN A,(0x%02X)",
-	"CALL C,0x%04X",0,"SBC A,0x%02X","RST 18",
-	"RET PE","POP HL","JP PE,0x%04X","EX (SP),HL", // E0
-	"CALL PE,0x%04X","PUSH HL","AND 0x%02X","RST 20",
-	"RET PO","JP (HL)","JP PO,0x%04X","EX DE,HL",
-	"CALL PO,0x%04X",0,"XOR 0x%02X","RST 28",
-	"RET P","POP AF","JP P,0x%04X","DI",
-	"CALL P,0x%04X","PUSH AF","OR 0x%02X","RST 30",
-	"RET M","LD SP,HL","JP M,0x%04X","EI",
-	"CALL M,0x%04X",0,"CP 0x%02X","RST 38"
+	"RET NZ","POP BC","JP NZ,#%.4X","JP #%.4X", // C0
+	"CALL NZ,#%.4X","PUSH BC","ADD A,#%2.2X","RST 00",
+	"RET Z","RET","JP Z,#%.4X",0,
+	"CALL Z,#%.4X","CALL #%.4X","ADC A,#%2.2X","RST 08",
+	"RET NC","POP DE","JP NC,#%.4X","OUT (#%2.2X),A", // D0
+	"CALL NC,#%.4X","PUSH DE","SUB #%2.2X","RST 10",
+	"RET C","EXX","JP C,#%.4X","IN A,(#%2.2X)",
+	"CALL C,#%.4X",0,"SBC A,#%2.2X","RST 18",
+	"RET PE","POP HL","JP PE,#%.4X","EX (SP),HL", // E0
+	"CALL PE,#%.4X","PUSH HL","AND #%2.2X","RST 20",
+	"RET PO","JP (HL)","JP PO,#%.4X","EX DE,HL",
+	"CALL PO,#%.4X",0,"XOR #%2.2X","RST 28",
+	"RET P","POP AF","JP P,#%.4X","DI",
+	"CALL P,#%.4X","PUSH AF","OR #%2.2X","RST 30",
+	"RET M","LD SP,HL","JP M,#%.4X","EI",
+	"CALL M,#%.4X",0,"CP #%2.2X","RST 38"
 };
 
 
@@ -503,9 +503,9 @@ void Desass(t_Memory mem, std::ostream &Listing, int Start, int Longueur )
 				{
 					strcpy( Inst, Chaine );
 					Chaine = Inst;
-					p = strstr( Inst, "%02X" );
+					p = strstr( Inst, "%2.2X" );
 					if ( p )
-						sprintf(Inst, Inst, Ad8);
+						sprintf(Inst, Inst, (unsigned char)Ad8);
 				}
 			}
 			else
@@ -516,7 +516,7 @@ void Desass(t_Memory mem, std::ostream &Listing, int Start, int Longueur )
 		if ( Chaine )
 		{
 			strcpy( Inst, Chaine );
-			p = strstr( Inst, "%04X" );
+			p = strstr( Inst, "%.4X" );
 			Ad16 = mem.Read(Adr++);
 			Ad16 += mem.Read(Adr) << 8;
 			Ad8 = ( char ) Ad16;
@@ -527,22 +527,22 @@ void Desass(t_Memory mem, std::ostream &Listing, int Start, int Longueur )
 			}
 			else
 			{
-				p = strstr( Inst, "%02X" );
+				p = strstr( Inst, "%2.2X" );
 				if ( p )
 				{
-					sprintf(Inst, Chaine, Ad8);
-					p = strstr( Inst, "%02X" );
+					sprintf(Inst, Chaine, (unsigned char)Ad8);
+					p = strstr( Inst, "%2.2X" );
 					if ( p )
 					{
-						sprintf(Inst, Chaine, Ad16 >> 8);
+						sprintf(Inst, Chaine, (unsigned char)(Ad16 >> 8));
 					}
 				}
 				else
 				{
-					p = strstr( Inst, "0xeeee " );
+					p = strstr( Inst, "#eeee " );
 					if ( p )
 					{
-						sprintf(p, "0x%04X", Adr + Ad8);
+						sprintf(p, "#%.4X", Adr + Ad8);
 					}
 					else
 						Adr--;
@@ -550,7 +550,7 @@ void Desass(t_Memory mem, std::ostream &Listing, int Start, int Longueur )
 			}
 		}
 		else
-			sprintf( Inst, "%02X %02X %02X ????", Instr, Inst2, Inst3 );
+			sprintf( Inst, "%2.2X %2.2X %2.2X ????", Instr, Inst2, Inst3 );
 
 		//Memory adress part
 		Listing.width(4);
@@ -566,7 +566,7 @@ void Desass(t_Memory mem, std::ostream &Listing, int Start, int Longueur )
 			Listing << ' ' ;
 			
 		}
-		for(;i<OldAdr+3;i++) Listing << "   ";
+		for(;i<OldAdr+4;i++) Listing << "   ";
 		Listing << "    " ;
 
 		// ASCII dump
@@ -577,6 +577,8 @@ void Desass(t_Memory mem, std::ostream &Listing, int Start, int Longueur )
 			if (val > 32 && val < 127)
 			{
 				Listing << (char) val ;
+				if (val=='&')
+					Listing << (char) val ;
 			}
 			else
 			{
