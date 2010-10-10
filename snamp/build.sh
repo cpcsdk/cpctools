2d210ede4136d7405f0ecbc3fe962a519ded638b
pulkomandy <pulkomandy@3b09e2c4-80d7-11dd-8b6f-79bb0735e950>
2010-10-10 20:49:44 +0000

    Remove no longer needed files from build script.

--- snamp/build.sh
+++ snamp/build.sh
@@ -1,5 +1,3 @@
 g++ -g main.cpp \
-../Reloaded/src/core/emulator.cpp  \
-../Reloaded/src/core/video.cpp  \
 ../Reloaded/src/core/libcapricecore.a  \
 -lportaudio -lstsound -lpthread -o snamp -I../Reloaded/src/core/ -I../Reloaded/
